e955112f5fe7572635337caa8f33d2cfa247eee4
ekko.cui <ekko.cui@gmail.com>
2020-08-11 19:18:59 +0800

    Spring Boot 集成 Redis 缓存

---
+++ springboot-redis/src/test/java/com/github/gleans/SpringBootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gleans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
